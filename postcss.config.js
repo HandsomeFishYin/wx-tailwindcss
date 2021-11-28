8129992ffa5228c0bcd6c4886a8993677998652c
xiangling <xiangling.yin@tuya.com>
2021-11-28 22:41:00 +0800

    init

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+const path = require('path')
+module.exports = {
+  plugins: [
+    require('autoprefixer')({
+      remove: process.env.UNI_PLATFORM !== 'h5'
+    }),
+    require('tailwindcss')({ config: './tailwind.config.js' }),
+  ]
+}
