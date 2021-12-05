9fcdfa4031415f161be1abf4d5583542bf7f9d1b
allen <294931812@qq.com>
2021-12-05 22:37:43 +0800

    refactor: edit script

--- script/buildTw.js
+++ script/buildTw.js
@@ -3,9 +3,9 @@ const { getParam } = require("./utils");
 
 const mode = getParam("m") || getParam("mode");
 
-const execScript = (filename, env) => {
+const execScript = (filename, env, params = "") => {
   return execSync(
-    `NODE_ENV=${env} npx tailwindcss -i srcStyles/common.css -p  -o ./styles/${filename}  && node script/replace -f ./styles/${filename}`,
+    `NODE_ENV=${env} npx tailwindcss -i srcStyles/common.css -p ${params}  -o ./styles/${filename}  && node script/replace -f ./styles/${filename}`,
     {
       stdio: "inherit",
     }
@@ -22,7 +22,7 @@ switch (mode) {
   case "pre":
   case "prod":
   case "production": {
-    execScript("tw.min.wxss", "production");
+    execScript("tw.min.wxss", "production", "-m");
     break;
   }
   default: {
